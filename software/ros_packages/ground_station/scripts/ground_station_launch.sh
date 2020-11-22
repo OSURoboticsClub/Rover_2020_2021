4d3439d2221f600c12a7586768ee146903f5a93d
FrankreedX <nguyanh2@oregonstate.edu>
2020-11-21 16:06:35 -0800

    shellscript can now recognize the src folder in ground_station. rover_arm can find msg headers

--- software/ros_packages/ground_station/scripts/ground_station_launch.sh
+++ software/ros_packages/ground_station/scripts/ground_station_launch.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-current_folder_name="scripts"
-current_folder_name_length=`expr length $current_folder_name`
+#current_folder_name="scripts"
+#current_folder_name_length=`expr length $current_folder_name`
 
 launch_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-launch_dir_length=`expr length $launch_dir`
+#launch_dir_length=`expr length $launch_dir`
 
-launch_dir_length_without_current_folder=$(($launch_dir_length-$current_folder_name_length))
+#launch_dir_length_without_current_folder=$(($launch_dir_length-$current_folder_name_length))
 
-script_launch_path="${launch_dir:0:$launch_dir_length_without_current_folder}/src"
-cd $script_launch_path
+#script_launch_path="${launch_dir:0:$launch_dir_length_without_current_folder}/src"
+
+script_launch_path="$launch_dir/src"
+
+cd "$script_launch_path" || exit
 
 cp ~/key .
 

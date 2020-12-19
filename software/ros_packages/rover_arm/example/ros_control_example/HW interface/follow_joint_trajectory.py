68503542471883a55ce64143eb184b13fa09a671
Kira Kopcho <kirak5683@gmail.com>
2020-12-19 10:48:46 -0800

    Updated ROS Control Files and added a WIP FollowJointTrajectory Action Server
    - Renamed file for easier editing (there was a space in the filename...)
    
    - Updated ROS control files in attempt to mitigate any namespace errors- it still appears we need HW interface/Gazebo for things to run properly
    
    - Added WIP files for Gazebo with arm
    
    - Added WIP FollowJointTajectory action server, still unsure if that's necessary but it's good practice

---
+++ software/ros_packages/rover_arm/example/ros_control_example/HW interface/follow_joint_trajectory.py
@@ -0,0 +1,9 @@
+# ! /usr/bin/env python
+
+import rospy
+import actionlib
+from control_msgs.msg import FollowJointTrajectoryAction
+from control_msgs.msg import FollowJointTrajectoryFeedback
+from control_msgs.msg import FollowJointTrajectoryResult
+from trajectory_msgs.msg import JointTrajectory
+

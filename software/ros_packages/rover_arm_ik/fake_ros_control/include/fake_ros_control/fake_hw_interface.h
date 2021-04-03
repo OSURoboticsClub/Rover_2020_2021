3bc0bae6ff6c782ff9d96ad9ba131f82bfbfa858
Kira Kopcho <kirak5683@gmail.com>
2021-04-03 14:22:03 -0700

    Started writing header file for fake hw interface

---
+++ software/ros_packages/rover_arm_ik/fake_ros_control/include/fake_ros_control/fake_hw_interface.h
@@ -0,0 +1,22 @@
+#ifndef FAKE_ROS_CONTROL_FAKE_HW_INTERFACE
+#define FAKE_ROS_CONTROL_FAKE_HW_INTERFACE
+
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <joint_limits_interface/joint_limits.h>
+#include <joint_limits_interface/joint_limits_interface.h>
+#include <joint_limits_interface/joint_limits_urdf.h>
+#include <controller_manager/controller_manager.h>
+#include <boost/scoped_ptr.hpp>
+#include <ros/ros.h>
+#include <urdf/model.h>
+#include <time.h>
+
+class FakeHWInterface : public hardware_interface::RobotHW {
+public:
+    FakeHWInterface(); //default constructor
+    FakeHWInterface(ros::NodeHandle& nh, urdf::Model* urdf_model = NULL);
+    ~FakeHWInterface();
+private:
+}

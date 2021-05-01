f68a2e2385d1fae4c0ba5b1b3db2cfbaa08fa762
Kira Kopcho <kirak5683@gmail.com>
2021-05-01 12:09:05 -0700

    Updated URDF and IKFast Plugin to have correct joint limits

--- software/ros_packages/rover_arm_ik/rover_arm_ikfast_arm_plugin/src/rover_arm_arm_ikfast_solver.cpp
+++ software/ros_packages/rover_arm_ik/rover_arm_ikfast_arm_plugin/src/rover_arm_arm_ikfast_solver.cpp
@@ -12,7 +12,7 @@
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
 ///
-/// ikfast version 0x10000049 generated on 2021-03-18 09:54:27.570656
+/// ikfast version 0x10000049 generated on 2021-05-01 11:59:27.094306
 /// To compile with gcc:
 ///     gcc -lstdc++ ik.cpp
 /// To compile without any main function as a shared object (might need -llapack):

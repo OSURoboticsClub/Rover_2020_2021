c768da739bff25aa917e19582808a3806ebe3e4d
Kira Kopcho <kirak5683@gmail.com>
2021-05-22 14:25:30 -0700

    Prelim controls file, still determining what messages/topics need to be sent to check if controller manager is running

---
+++ software/ros_packages/ground_station/src/Framework/ArmSystems/ArmIKControls.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+#####################################
+# Imports
+#####################################
+# Python native imports
+from PyQt5 import QtCore, QtWidgets, QtGui
+import logging
+import rospy
+
+# put msg files here when ready
+
+#####################################
+# Global Variables
+#####################################
+ARM_JOINT_STATUS = /rover_arm/arm_controller/state 

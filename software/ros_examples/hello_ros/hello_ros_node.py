b2cda8c4913fde183f0c40dac50dee37a8fa0e74
Kira Kopcho <kirak5683@gmail.com>
2021-10-20 22:21:41 -0700

    Slightly modified hello_ros node while teaching people how to write nodes

--- software/ros_examples/hello_ros/hello_ros_node.py
+++ software/ros_examples/hello_ros/hello_ros_node.py
@@ -2,12 +2,15 @@
 import rospy
 
 def hello_ros_node():
-    rospy.init_node('hello_ros') #establishes the program as a ros node
+    rospy.init_node('hello_ros')
 
-    rospy.loginfo("Hello ROS!") #prints a message to the terminal using the ROS_INFO stream
+    rospy.loginfo("Hello ROS!")
 
 if __name__ == '__main__':
     try:
         hello_ros_node()
     except rospy.ROSInterruptException:
         pass
+
+
+
